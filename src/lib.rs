677181b9fca8ca3abb54ac05ca6557473aaa9564
Dragnansia <50181744+Dragnansia@users.noreply.github.com>
2022-02-21 15:14:07 +0100

    ProtonGE api url on lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,6 @@ pub mod archive;
 pub mod cache;
 pub mod downloader;
 pub mod error;
+
+pub const PROTONGE_API_URL: &str =
+    "https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases";
