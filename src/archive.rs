5aaae654ee9d5c0c42f042d53386044c0add3bd7
Dragnansia <50181744+Dragnansia@users.noreply.github.com>
2022-02-21 15:13:37 +0100

    add archive install function

---
+++ src/archive.rs
@@ -0,0 +1,11 @@
+use crate::error::Error;
+use std::fs::File;
+use tar::Archive;
+
+pub fn install(file_path: &str, install_path: &str) -> Result<(), Error> {
+    let archive = File::open(file_path)?;
+    let mut archive = Archive::new(&archive);
+    archive.unpack(install_path)?;
+
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+pub mod archive;
 pub mod cache;
 pub mod downloader;
 pub mod error;
